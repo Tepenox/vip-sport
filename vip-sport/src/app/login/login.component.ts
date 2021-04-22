dbec49e0756b4d1cbbe28e72e142323892c5fe57
Dorian <vdoralex@gmail.com>
2021-04-22 22:40:59 +0200

    adding email and password verification

--- vip-sport/src/app/login/login.component.ts
+++ vip-sport/src/app/login/login.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
   constructor(private formBuilder: FormBuilder) {}
@@ -17,7 +21,10 @@ export class LoginComponent implements OnInit {
 
   initForm() {
     this.loginForm = this.formBuilder.group({
-      email: ['', Validators.required],
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+      ]),
       password: ['', Validators.required],
     });
   }
