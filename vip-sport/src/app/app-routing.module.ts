8627d24886bc4b774ec8f6c09cde013593b21a1d
Dorian <vdoralex@gmail.com>
2021-04-23 14:47:18 +0200

    adding navigation

--- vip-sport/src/app/app-routing.module.ts
+++ vip-sport/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component : WelcomeComponent},
+  {path: 'welcome', component : WelcomeComponent},
+  {path: 'profile', component : ProfileComponent},
+  {path: 'login', component : LoginComponent},
+  {path: 'signup', component : SignUpComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
