fdeb000dd8e39cb0caeabeb2126fb1bd5f3c778e
Dorian <vdoralex@gmail.com>
2021-04-29 17:02:33 +0200

    ajout modification du profil

--- vip-sport/src/app/profile/profile.component.ts
+++ vip-sport/src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/User';
 
 @Component({
   selector: 'app-profile',
@@ -6,20 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent implements OnInit {
-  infos = [
-    {
-      age: '21',
-      sport: 'Vélo',
-      citation: 'Go Muscu',
-      weigth: '80',
-    },
-  ];
-  moreInfos = [
-    { content: 'Regime haute protéine' },
-    { content: 'Sportif monstreux' },
-  ];
+  profileModification: boolean = false;
+
+  user: User = {
+    id: 1,
+    name: 'Xx_DarkMage69_xX',
+    age: 27,
+    sport: 'Sègue',
+    citation: 'Trop kawaï ^^',
+    weigth: 120,
+    height: 175,
+    profilePicturePath: '../assets/pp.png',
+    description:
+      'Puceau moi ? Sérieusement ^^ haha on me l avait pas sortie celle la depuis loooongtemps 🙂 demande a mes potes si je suis puceau tu vas voir les réponses que tu vas te prendre XD rien que la semaine passee j ai niquer dont chuuuuut ferme la puceau de merde car toi tu m as tout tout l air d un bon puceau de merde car souvent vous etes frustrer de ne pas BAISER 🙂 ses agreable de se faire un missionnaire ou un amazone avec une meuf hein ? tu peux pas répondre car tu ne sais pas ce que c ou alors tu le sais mais tu as du taper dans ta barre de recherche « missionnaire sexe » ou « amazone sexe » pour comprendre ce que c etait mdddrrr !! cest grave quoiquil en soit....pour revenir a moi, je pense que je suis le mec le moins puceau de ma bande de 11 meilleurs amis pas psk j ai eu le plus de rapport intime mais psk j ai eu les plus jolie femme que mes amis 😀 ses pas moi qui le dit, ses eux qui commente sous mes photos insta « trop belle la fille que tu as coucher avec hier en boite notamment! » donc après si tu veux',
+  };
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  enableProfileModif() {
+    this.profileModification = !this.profileModification;
+  }
+
+  saveModification() {
+    this.profileModification = !this.profileModification;
+  }
 }
