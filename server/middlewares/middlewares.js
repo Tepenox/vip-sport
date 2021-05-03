1ff71d4379bedd57fe6a558a62157272f0028817
Tepenox <anasselafya@gmail.com>
2021-05-03 18:01:53 +0200

    fixed bugs

--- server/middlewares/middlewares.js
+++ server/middlewares/middlewares.js
@@ -9,16 +9,16 @@ middlewares.verifyToken = function verifyToken(req, res, next) {
     if(!req.headers.authorization) {
       return res.status(401).send('Unauthorized request')
     }
-    let token = req.headers.authorization.split(' ')[1]
-    console.log(token);
+    let token = req.headers.authorization.split(' ')[1];
     if(token === 'null') {
       return res.status(401).send('Unauthorized request')    
     }
-    let payload = jwt.verify(token, Token.hashKey);
-    if(!payload) {
-      return res.status(401).send('Unauthorized request')    
+    try{
+      let payload = jwt.verify(token, Token.hashKey);
+      req.userId = payload.subject;  
+    }catch{
+      return res.status(401).send('Unauthorized request');    
     }
-    req.userId = payload.subject
     next()
   }
 
--- server/models/token.js
+++ server/models/token.js
@@ -1,3 +1,4 @@
+let fs = require('fs');
 let hashKey = "ammooggoosee";
 
 let token ={};
