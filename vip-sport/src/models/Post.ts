32f186f320df2ff0eef20aab1530bbe357a3d6ea
Tepenox <anasselafya@gmail.com>
2021-05-05 17:35:05 +0200

    implemented post routes as a service

---
+++ vip-sport/src/models/Post.ts
@@ -0,0 +1,15 @@
+export class Post{
+    constructor(
+        public title:string, 
+        public contentType:string, 
+        public content:string, 
+        public contentUrl:string, 
+        public onwerId:number, 
+        public categories:string, 
+        public date?:string,
+        public id?:string
+     
+    ){
+
+    }
+}
