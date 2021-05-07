9e88874555862eacb3187fd9e064c6216221d189
t17000929 <benjamin.taguet@etu.univ-amu.fr>
2021-05-07 17:54:16 +0200

    Implémentation partielle de réponse

--- vip-sport/src/app/post-form/post-form.component.ts
+++ vip-sport/src/app/post-form/post-form.component.ts
@@ -1,6 +1,7 @@
 import { Content } from '@angular/compiler/src/render3/r3_ast';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms'
+import { ForumPostService } from '../services/forum-post.service';
 
 @Component({
   selector: 'post-form',
@@ -8,6 +9,8 @@ import { FormGroup, FormControl, Validators } from '@angular/forms'
   styleUrls: ['./post-form.component.css']
 })
 export class PostFormComponent {
+  @Input('threadID') threadID: number;
+
   characterLimit = 2000;
   form = new FormGroup({
     postContent: new FormControl('', [
@@ -19,7 +22,19 @@ export class PostFormComponent {
   content = '';
   contentLength = this.content.length;
 
+  constructor(private service: ForumPostService) { }
+
   get postContent() {
     return this.form.get('postContent');
+  }
+
+  submitPost() {
+    //Placeholder: il faut récupérer l'id de l'utilisateur connecté
+    let post = { thread: this.threadID, user: 3, date: "2021-01-29 16:02:08", content: this.content };
+    this.service.create(post)
+      .subscribe((response: { id }) => {
+        post['id'] = response.id;
+        console.log(response);
+      })
   }
 }
