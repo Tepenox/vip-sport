f86c46006c7d06eb8dd7999da8f1eecb6bb74ffb
t17000929 <benjamin.taguet@etu.univ-amu.fr>
2021-05-10 19:46:20 +0200

    Affichage des information du thread dans la liste des sujets

--- vip-sport/src/app/thread-link/thread-link.component.ts
+++ vip-sport/src/app/thread-link/thread-link.component.ts
@@ -1,7 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Thread } from 'src/models/Thread';
+import { User } from 'src/models/User';
 import { ThreadService } from '../services/thread.service';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'thread-link',
@@ -12,15 +14,23 @@ export class ThreadLinkComponent implements OnInit {
   @Input('threadId') id: number;
   subcategoryID: number;
   thread: Thread;
+  author: User;
 
-  constructor(private route: ActivatedRoute, private service: ThreadService) { }
+  constructor(private route: ActivatedRoute, private threadService: ThreadService, private userService: UserService) { }
 
   ngOnInit(): void {
     this.route.paramMap
       .subscribe(params => this.subcategoryID = +params.get('subcategoryID'));
 
-    this.service.getById(this.id)
-      .subscribe((response: Thread[]) => this.thread = response[0]);
+    this.threadService.getById(this.id)
+      .subscribe((response: Thread) =>  {
+        this.thread = response[0];
+        this.userService.getUserById(this.thread.ownerId)
+          .subscribe((response: User) => { 
+            this.author = response;
+            console.log(response);
+          });
+      });
   }
 
 }
--- vip-sport/src/models/Thread.ts
+++ vip-sport/src/models/Thread.ts
@@ -3,6 +3,8 @@ export class Thread {
         public id: number,
         public subcategoryId: number,
         public title: String,
+        public ownerId: number,
+        public date: String,
         public firstPostId: number,
         public lastPostId: number
     ) { }
