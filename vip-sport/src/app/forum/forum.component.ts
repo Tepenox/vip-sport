10a75c0cb132beaa61ba8d417a911067ae904e47
t17000929 <benjamin.taguet@etu.univ-amu.fr>
2021-05-20 04:33:49 +0200

    Ajout de titres d'onglet

--- vip-sport/src/app/forum/forum.component.ts
+++ vip-sport/src/app/forum/forum.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { Category } from 'src/models/Category';
 import { Subcategory } from 'src/models/Subcategory';
@@ -13,26 +14,27 @@ import { ThreadService } from '../services/thread.service';
   styleUrls: ['./forum.component.css']
 })
 export class ForumComponent implements OnInit {
-  currentCategoryId: number;
+  currentSubcategory: Subcategory;
   categories: Category[];
   subcategories : Subcategory[][] = Array(0);
   threads: Thread[];
   isThreadFormVisible: boolean = false;
 
-  constructor(private route: ActivatedRoute, private categoriesService: CategoriesService, private subcategoriesService: SubcategoriesService, private threadService: ThreadService) { }
+  constructor(private route: ActivatedRoute, private threadService: ThreadService, private titleService: Title) { }
 
   ngOnInit(): void {
-    this.route.paramMap
-      .subscribe(params => {
-        this.currentCategoryId = +params.get('subcategoryID');
-      });
-
-    this.route.data.subscribe(response => {
+    this.route.data.subscribe((response: { categories: Category[], subcategories: Subcategory[][], currentSubcategory: Subcategory } ) => {
       this.categories = response.categories;
+      this.currentSubcategory = response.currentSubcategory;
       this.subcategories = response.subcategories;
+
+      console.log(response);
+
+      if (this.currentSubcategory.id != 0)
+      this.titleService.setTitle(this.titleService.getTitle() + " " + this.currentSubcategory.name)
     });
 
-    this.threadService.getByParentId(this.currentCategoryId)
+    this.threadService.getByParentId(this.currentSubcategory.id)
       .subscribe((response: Thread[]) => {
         this.threads = response;
       });
